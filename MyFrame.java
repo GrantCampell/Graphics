c8ba4efd8cdd36179bd3c01c59a6fa6a33263b3f
Grant Campbell <Grant.D.Campbell-1@ou.edu>
2018-11-14 14:37:02 -0600

    Add a rectangle

--- MyFrame.java
+++ MyFrame.java
@@ -16,7 +16,7 @@ public class MyFrame extends JFrame
         setVisible(true);
 
     }
-    
+
     public void paintLine()
     {
         panel = new LineDemoPanel();
@@ -24,10 +24,20 @@ public class MyFrame extends JFrame
         setVisible(true);
     }
 
+    public void paintRectangle()
+    {
+        panel = new RectangleDemoPanel();
+        add(panel);
+        setVisible(true);
+    }
+
     public static void main(String[] args)
     {
-        MyFrame frame = new MyFrame("Hello");
+        MyFrame frame = new MyFrame("Line");
         frame.paintLine();
+
+        MyFrame frame1 = new MyFrame("Rectangle");
+        frame1.paintRectangle();
     }
 
 }
